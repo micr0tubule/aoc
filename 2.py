b4f361fcefaf1dae63a8dd95d9b34a1bc620bfc0
micr0tubule <d4v1dbraun@gmail.com>
2021-01-24 06:04:37 +0100

    asdf

--- 2.py
+++ 2.py
@@ -1,13 +1,33 @@
-with open('2') as f:
-    passwords = [password.split(':') for password in f.readlines()]
+with open('2') as f: 
+    input = [line.split() for line in f.readlines()]
 
+
+# part 1
 count = 0
-for condition, password in passwords: 
-    num, letter = condition.split()
-    start, end = num.split('-')
-    start, end = int(start), int(end)
-    letter_count = password.count(letter)
-    if start <= letter_count <= end: 
+for i in input: 
+    condition, letter, pw = i
+    begin, end = condition.split('-')
+    begin, end = int(begin), int(end)
+    letter = letter[0]
+    if begin <= pw.count(letter) <= end: 
         count += 1 
+
+# part 2 
+count = 0 
+for i in input: 
+    condition, letter, pw = i 
+    pos1, pos2 = condition.split('-')
+    pos1, pos2 = int(pos1), int(pos2)
+    letter = letter[0]
+    try: 
+        if pw[pos1-1] == letter and not pw[pos2-1] == letter: 
+            count += 1 
+        if pw[pos2-1] == letter and not pw[pos1-1] == letter:
+            count += 1 
+    except IndexError as e: 
+        print(pos2)
+        print(pw)
+        pass  
 print(count)
+
 
